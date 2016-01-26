3b9041054a309de2c872e981ac41dbb831de6087
Jeff <jgregory@bitcasa.com>
2016-01-26 15:44:29 -0800

    spa

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -81,8 +81,9 @@ angular.module('confusionApp')
 	};
 }])
 
-.controller('DishDetailController', ['$scope', 'menuFactory', function ($scope, menuFactory) {
-	$scope.dish = menuFactory.getDish(3);
+.controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function ($scope, $stateParams, menuFactory) {
+	var dish = menuFactory.getDish(parseInt($stateParams.id, 10));
+	$scope.dish = dish;
 }])
 
 .controller('DishCommentController', ['$scope', function ($scope) {
@@ -118,6 +119,4 @@ angular.module('confusionApp')
 
 
 	}
-}])
-
-;
+}]);
